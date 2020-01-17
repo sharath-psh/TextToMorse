35e40af5db9f9e65ff56a4cf6a7a81ba2d3338aa
sharath-psh <sharath.psh1@gmail.com>
2020-01-17 19:43:06 +0530

    caps

--- tm.py
+++ tm.py
@@ -1,10 +1,7 @@
 import pyautogui
 import morse_talk as mtalk
 
-# print(pyautogui.KEYBOARD_KEYS)
-
-pyautogui.keyDown('fn')
-pyautogui.press('f1')
-
 mcode = mtalk.encode('Sharath', encoding_type='binary')
 print(mcode)
+
+pyautogui.press('f1')
