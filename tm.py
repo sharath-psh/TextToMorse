d7fddc7b3aeef59234d9eae308e1aaf0a864fb74
sharath-psh <sharath.psh1@gmail.com>
2020-01-17 19:05:41 +0530

    added pyautogui

--- tm.py
+++ tm.py
@@ -1,3 +1,10 @@
+import pyautogui
 import morse_talk as mtalk
+
+# print(pyautogui.KEYBOARD_KEYS)
+
+pyautogui.keyDown('fn')
+pyautogui.press('f1')
+
 mcode = mtalk.encode('Sharath', encoding_type='binary')
 print(mcode)
