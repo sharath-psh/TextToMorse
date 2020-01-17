b18b1ab84d1c53cb072528727c2a0b290c0ea9b1
sharath-psh <sharath.psh1@gmail.com>
2020-01-17 19:59:34 +0530

    list

--- tm.py
+++ tm.py
@@ -1,18 +1,13 @@
 import pyautogui
 import morse_talk as mtalk
 
-mcode = mtalk.encode('Sharath', encoding_type='binary')
+txt = "code"
+
+mcode = mtalk.encode(txt, encoding_type='binary')
 print(mcode)
 
-
-
-# Python3 program to Split string into characters 
-def split(word): 
-	return [char for char in word] 
-	
-# Driver code 
-word = 'geeks'
-print(split(word)) 
+txt = list(txt)
+print(txt)
 
 
 # pyautogui.press('f1')
